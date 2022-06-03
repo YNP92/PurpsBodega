b783769e0125d0186f2a0dfa0cee6a681c5776f7
austin <austinzaia97@gmail.com>
2022-06-03 19:27:20 -0400

    commit for all new changes

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,6 +1,8 @@
 
 public class Inventory extends ShoppingCart {
 
+	public static final int TAX_RATE = 0;
+
 	public Inventory(Product product, int quantity) {
 		// TODO Auto-generated constructor stub
 	}
@@ -8,6 +10,21 @@ public class Inventory extends ShoppingCart {
 	public int getQuantity() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Object get(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object get(int i) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
