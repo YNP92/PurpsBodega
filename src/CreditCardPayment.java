8a06b6a1040f00b9fb0242f1cccc7baf790155f5
tristanbeatty95 <tristanbeatty@ymail.com>
2022-06-04 20:28:11 -0400

    Added exit out of cc payment method

--- src/CreditCardPayment.java
+++ src/CreditCardPayment.java
@@ -3,8 +3,10 @@ public class CreditCardPayment extends Payment {
 
 	private String creditCardNumber, expirationDate, cVV;
 	int validationCount = 0;
+	boolean usercont = true;
 
 	public void pay() {
+		while(usercont) {
 		System.out.println("Please enter your credit card number: ");
 		creditCardNumber = UserInput.stringGet();
 		if (validateCreditCardNumber(creditCardNumber) == true) {
@@ -22,6 +24,11 @@ public class CreditCardPayment extends Payment {
 		}
 		if (validationCount == 3) {
 			System.out.println("Card accepted, thank you for your business!");
+			usercont = false;
+			break;
+		} else {
+			System.out.println("Your card information was invalid, please try again.");
+		}
 		}
 	}
 
